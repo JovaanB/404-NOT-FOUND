009dc6874dc6fd35eb30fcda2b1df368ec971cdb
Jovan <jbienvenu@wisersoftware.com>
2020-07-14 14:11:25 +0200

    Updates

---
+++ precache-manifest.a048018b2c1e61fdd52ba653ef919fbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5d87d80ed4e063e6f80422aeba7d38",
+    "url": "/404-NOT-FOUND/index.html"
+  },
+  {
+    "revision": "6d7214103a20114c99c8",
+    "url": "/404-NOT-FOUND/static/css/main.9606fbbb.chunk.css"
+  },
+  {
+    "revision": "d39ce45688d54358f7d1",
+    "url": "/404-NOT-FOUND/static/js/2.ab695456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404-NOT-FOUND/static/js/2.ab695456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7214103a20114c99c8",
+    "url": "/404-NOT-FOUND/static/js/main.ec154aa7.chunk.js"
+  },
+  {
+    "revision": "ef715080031ae46af194",
+    "url": "/404-NOT-FOUND/static/js/runtime-main.4282d84b.js"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/404-NOT-FOUND/static/media/scarecrow.9f720a55.png"
+  }
+]);
